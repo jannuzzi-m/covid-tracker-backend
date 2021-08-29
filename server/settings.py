a2dfb0169e291b2511807349b07ef1fc3115f788
Marcelino Jannuzzi <jannuzzimarcelino1@gmail.com>
2021-08-29 19:57:27 -0300

    allowed all hosts to connect

--- server/settings.py
+++ server/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4!rmm4t9qob=t3l+9e6@jnvglwo8x&q)#%vf03p^hcy6&abjhn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
